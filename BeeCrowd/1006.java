08adf299b7c43eaaa895742e6b228d052efd54ad
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2024-10-29 22:26:08 +0000

    Initial Commit

---
+++ BeeCrowd/1006.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner; 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    
+    double A = scanner.nextDouble();
+    double B = scanner.nextDouble();
+    double C = scanner.nextDouble();
+    
+    double MEDIA = ((A * 2) + (B * 3) + (C * 5))/10;
+    
+    System.out.printf("MEDIA = %.1f%n" , MEDIA);
+    scanner.close();
+    
+    scanner.close();
+    }
+ 
+}
